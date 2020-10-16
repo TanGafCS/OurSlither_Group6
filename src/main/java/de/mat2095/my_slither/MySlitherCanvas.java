c15c84aef7056272147a3bd45639ac6daa35f438
ejazmush <t.ejazmushtaq@lancaster.ac.uk>
2020-10-16 16:23:25 +0100

    Tried to change colour

--- src/main/java/de/mat2095/my_slither/MySlitherCanvas.java
+++ src/main/java/de/mat2095/my_slither/MySlitherCanvas.java
@@ -27,11 +27,13 @@ final class MySlitherCanvas extends JPanel {
     private static final Color[] PREY_HALO_COLORS = new Color[]{new Color(0x60FFFF00, true), new Color(0x00FFFF00, true)};
     private static final Color SNAKE_COLOR = new Color(0x287BDE);
     private static Color OWN_SNAKE_COLOR = new Color(0x39AFFF);
+    private static Color NEW_SNAKE_COLOR = new Color(0xff0000);
     private static final float[] SNAKE_HALO_FRACTIONS = new float[]{0.5f, 1f};
     private static final Color[] SNAKE_HALO_COLORS = new Color[]{new Color(0x60287BDE, true), new Color(0x00287BDE, true)};
     private static final Color[] OWN_SNAKE_HALO_COLORS = new Color[]{new Color(0x6039AFFF, true), new Color(0x0039AFFF, true)};
     private static final Color SNAKE_BODY_COLOR = new Color(0x6A8759);
     private static Color OWN_SNAKE_BODY_COLOR = new Color(0xA5C261);
+    private static Color NEW_SNAKE_BODY_COLOR = new Color(0xff0000);
     private static final Color MAP_COLOR = new Color(0xA0A9B7C6, true);
     private static final Color MAP_POSITION_COLOR = new Color(0xE09E2927, true);
     private static final Color NAME_SHADOW_COLOR = new Color(0xC02B2B2B, true);
@@ -244,6 +246,13 @@ final class MySlitherCanvas extends JPanel {
                 g.fill(new Ellipse2D.Double(snake.x - thickness * 2 / 3, snake.y - thickness * 2 / 3, thickness * 4 / 3, thickness * 4 / 3));
 
                 String lengthText = "" + model.getSnakeLength(snake.body.size(), snake.getFam());
+
+                double snakeL = model.getSnakeLength(snake.body.size(), snake.getFam());
+                System.out.println(snakeL);
+                if(snakeL > 110) {
+                    OWN_SNAKE_COLOR = new Color(0xff0000);
+                    OWN_SNAKE_BODY_COLOR = new Color(0xff0000);
+                }
 
                 g.setColor(NAME_SHADOW_COLOR);
                 g.drawString(snake.name,
